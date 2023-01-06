9d2b5caf5634672dcee2eabe3c8822fa0f05bdb4
Messai <messai@github.com>
2023-01-06 09:34:40 +0300

    correction to 0"

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-import add_o as add
-a = 1
-b = 2
-print("{} + {} = {}".format(add.a, add.b, add.add(a, b)))
+if __name__ = "__main__"
+    import add_o as add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(add.a, add.b, add.add(a, b)))
