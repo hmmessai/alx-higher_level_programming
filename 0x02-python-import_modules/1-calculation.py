ad4e136e3301164b21f73d330a15936084d39bc2
Messai <messai@github.com>
2023-01-06 09:54:35 +0300

    correction to 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     b = 5
 
     print("{} + {} = {}".format(a, b, calc.add(a, b)))
-    print("{} - {} = {}".format(a, b, calc.sub(a,b)))
+    print("{} - {} = {}".format(a, b, calc.sub(a, b)))
     print("{} * {} = {}".format(a, b, calc.mul(a, b)))
     print("{} / {} = {}".format(a, b, calc.div(a, b)))
