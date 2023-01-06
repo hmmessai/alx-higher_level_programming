01768e3f26bf684b0d2ee129d31258a10ae981d1
Messai <messai@github.com>
2023-01-06 09:58:36 +0300

    correction to 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import sys
-count = 1
-argv = sys.argv
-arg_num = len(argv)
+if __name__ == "__main__":
+    import sys
+    count = 1
+    argv = sys.argv
+    arg_num = len(argv)
 
-print("{} argument{}{}".format(arg_num - 1,"" if arg_num == 2 else "s" , "." if arg_num == 1 else ":"))
-for i in range(arg_num - 1):
-    print("{}: {}".format(count, argv[count]))
-    count += 1
+    print("{} argument{}{}".format(arg_num - 1,"" if arg_num == 2 else "s" , "." if arg_num == 1 else ":"))
+    for i in range(arg_num - 1):
+        print("{}: {}".format(count, argv[count]))
+        count += 1
