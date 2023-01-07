5a3279233ca81c705219a508c60c169c029a79ec
Messai <messai@github.com>
2023-01-07 11:16:35 +0300

    Added 102-calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,9 +3,9 @@ def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
-    for i in range(4, 6):
-        c = add(c, i)
+        for i in range(4, 6):
+            c = add(c, i)
         return(c)
-    return(sub(a, b))
-return(0)
+    else:
+        return(sub(a, b))
 
