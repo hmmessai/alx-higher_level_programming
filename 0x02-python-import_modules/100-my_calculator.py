6cc3570862b231332a3dc82a1ef3d282373fa694
Messai <messai@github.com>
2023-01-07 08:44:39 +0300

    Added 100-my_calculator.py

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+    args = sys.argv
+    if len(args) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    operators = ["+", "-", "*", "/"]
+    if args[2] not in opertors:
+        print("Unkown operator. Available operators: +, -, * and /")
+        exit(1)
+    a = int(args[1])
+    b = int(args[2])
+    if args[2] == "+":
+        print("{} {} {} = {}".format(a, operator, b, add(a, b)))
+    elif args[2] == "-":
+        print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
+    elif args[2] == "*":
+        print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
+    elif args[2] == "/":
+        print("{} {} {} = {}".format(a, operator, b, div(a, b)))
