e1daccd6a11902463fa7c6d8ea565c8c175b6819
Messai <messai@github.com>
2023-01-07 08:52:40 +0300

    Added 100-my_calc.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,16 +7,17 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     operators = ["+", "-", "*", "/"]
-    if args[2] not in opertors:
+    if args[2] not in operators:
         print("Unkown operator. Available operators: +, -, * and /")
         exit(1)
     a = int(args[1])
-    b = int(args[2])
-    if args[2] == "+":
-        print("{} {} {} = {}".format(a, operator, b, add(a, b)))
-    elif args[2] == "-":
+    b = int(args[3])
+    operator = args[2]
+    if operator == "+":
+        print("{} {} {} = {}".format(a, operator , b, add(a, b)))
+    elif operator == "-":
         print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
-    elif args[2] == "*":
+    elif operator == "*":
         print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
-    elif args[2] == "/":
+    elif operator == "/":
         print("{} {} {} = {}".format(a, operator, b, div(a, b)))
