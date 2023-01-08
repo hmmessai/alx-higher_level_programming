02bbfa7b775288b4e2082fae11f67468324b76ef
Messai <messai@github.com>
2023-01-08 10:51:52 +0300

    Added 13-insert_number.c and lists.h

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -23,7 +23,7 @@ listint_t *insert_node(listint_t **head, int number)
 		return (new);
 	}
 	
-	while (node->next->n < number && node->next && node)
+	while (node && node->next && node->next->n < number)
 		node = node->next;
 
 	new->next = node->next;
