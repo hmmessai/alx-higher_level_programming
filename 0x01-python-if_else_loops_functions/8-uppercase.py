7e2c3bd488431327701f3b8ece6385625a5754cc
Messai <messai@github.com>
2023-01-08 10:00:58 +0300

    correction to 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     for i in range(len(str) - 1):
         if ord(str[i]) <= 122 and ord(str[i]) >= 97:
-            c = chr(ord(str[i]))
+            c = chr(ord(str[i]) - 32)
             print("{}".format(c), end="")
         else:
             print("{}".format(str[i]), end="")
