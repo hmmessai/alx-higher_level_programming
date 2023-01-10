95984a6645835a20460b6d1c28be9c4cba700ad3
Messai <messai@github.com>
2023-01-10 10:36:36 +0300

    Added 5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+
+    for c in my_string:
+        if c == 'c' or c == 'C':
+            pass
+        else:
+            new_string += c
+    
+    return new_string
