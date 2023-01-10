b620a014e03d7c23aa9b1fd7e2bb54bbf15d4e75
Messai <messai@github.com>
2023-01-10 09:18:09 +0300

    Added 3-print_reversed_list_integer.py

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    width = len(my_list) -1
+    for i in range(width + 1):
+        num = my_list[width - i] 
+        print("{}".format(num))
