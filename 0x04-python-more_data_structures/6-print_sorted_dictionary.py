9768c326ae0e71dea84585f5a1c7a41885f42764
Messai <messai@github.com>
2023-01-11 16:07:40 +0300

    Added 9-multiply_by_2.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    a_list = sorted(list(a_dictionary.keys()))
+    a_list = list(a_dictionary.keys())
+    a_list.sort()
 
     for i in a_list:
         print("{} : {}".format(i, a_dictionary.get(i)))
