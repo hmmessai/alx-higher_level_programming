b3880e9ad4a7e489eef20512e07ba969e700d5e7
Messai <messai@github.com>
2023-01-24 21:35:38 +0300

    Added 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+A class that defines square by size
+"""
+
+
+class Square:
+    """
+    A class that defines a square by size
+    """
+    def __init__(self, size):
+        self.__size = size
