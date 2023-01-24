9f798eeaaea0a17851198404b2f3542c286f81cb
Messai <messai@github.com>
2023-01-24 22:04:27 +0300

    Added 2 & 3-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 """
-Defines a square class
 """
-
-
 class Square:
     """Represents a square"""
 
-    def __init__(self, size=0):
-        """Initialize a new square
+
+    def __init__(self, value):
+        """Initializes a new square object
 
         Args:
-            size(int): The size of the new square.
+            size: size of the square
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        """
