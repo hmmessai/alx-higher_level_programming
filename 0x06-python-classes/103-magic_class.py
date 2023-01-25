660b6d577a594082538d8ebcc7a6a05f139412cb
Messai <messai@github.com>
2023-01-25 19:49:01 +0300

    Added 103-magic_class.py

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-from math import pi
 """Define a MagicClass"""
+
+import math
 
 
 class MagicClass:
-    """Representation of MagicClass"""
+    """Representation of circle"""
 
     def __init__(self, radius=0):
-        """Initialize the MagicClass object
+        """Initialize the circle object
 
         Arg:
-            radius(int / float): the radius of the MagicClass object
+            radius(int or float): the radius of the circle object
         """
         if (not isinstance(radius, float) or not isinstance(radius, int)):
             raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
-        """Computes the area of the radius"""
-        return (pi * self.__radius ** 2)
+        """Computes the area of the circle"""
+        return (math.pi * self.__radius ** 2)
 
     def circumference(self):
         """Computes the circumference of the circle"""
-        return (self.__radius * pi * 2)
+        return (self.__radius * math.pi * 2)
