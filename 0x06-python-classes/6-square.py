d6a9eab70b5bd206f45aa1657e35b50c791ed454
Messai <messai@github.com>
2023-01-25 18:03:57 +0300

    Added 5 and 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -7,12 +7,14 @@ Define a square class
 class Square:
     """Represent a square"""
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """Initializes a square class
         Args:
             size(int): The size of the new square
+            position(int, int): The position of the new square
         """
         self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -32,6 +34,19 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
     def area(self):
         """Computes area of the square"""
         return self.__size ** 2
@@ -40,8 +55,13 @@ class Square:
         """Prints # characters on stdout"""
         if self.__size == 0:
             print()
+            return
+        for i in range(0, self.__position[1]):
+            print()
         else:
             for i in range(self.__size):
+                for k in range(0, self.__position[0]):
+                    print(" ", end="")
                 for j in range(self.__size):
                     print("#", end="")
                 print()
