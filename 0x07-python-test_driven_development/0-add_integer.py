2e2436d5c291a766927768febb425aeee0213889
Messai <messai@github.com>
2023-01-28 09:52:51 +0300

    Added 2-matix_divided.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
+"""Defines an integer addition function"""
+
+
 def add_integer(a, b=98):
+    """Return the integer addition of a and b.
+
+    Float arguments are typecasted to ints before addition is performed.
+
+    Raises:
+        TypeError: If either of a or b is a non-integer and non-float.
+    """
     if type(a) not in [int, float]:
-        raise TypeError("a mst be an integer")
+        raise TypeError("a must be an integer")
     elif type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    if type(a) is float:
-        a = int(a)
-    elif type(b) is float:
-        b = int(b)
-    return (a + b)
+    return (int(a) + int(b))
