6725180435c420b10edf2a0f80119418123ee3b1
Messai <messai@github.com>
2023-02-06 17:30:04 +0300

    Added 4-inherits_from.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -3,5 +3,13 @@
 
 
 def is_kind_of_class(obj, a_class):
-    """Checks if obj is instance of a_class or any inherited class."""
+    """Checks if obj is instance of a_class or any inherited class.
+
+    Args:
+        obj: the object to be checked.
+        a_class: the class to be checked if obj is instance.
+    Return:
+        True - if obj is in fact instance of a_class.
+        False - otherwise.
+    """
     return isinstance(obj, a_class)
---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a function inherits_from."""
+
+def inherits_from(obj, a_class):
+    """Checks if the obj is an inherited instance of a_class.
+
+    Args:
+        obj: the object to be checked.
+        a_class: the class to be checked.
+    Return:
+        True - if the obj is inherited from a_class.
+        False - otherwise.
+    """
+    return issubclass(type(obj), a_class) and type(obj) != a_class
