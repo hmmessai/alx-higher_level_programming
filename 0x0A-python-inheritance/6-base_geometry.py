796bd5d480f9bd2fbd0d70fdddc24f682bb67877
Messai <messai@github.com>
2023-02-06 17:36:16 +0300

    Added 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines the class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represents the class BaseGeometry."""
+    def area(self):
+        raise Exception(area() is not implemented)
