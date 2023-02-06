079c7c2d75f39eca834a53383fe8a79aaca21e7f
Messai <messai@github.com>
2023-02-06 16:25:17 +0300

    Added 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-"""Defines print_sorted function."""
+"""Defines MyList class."""
 
 
-def print_sorted(self):
-    
+class MyList(list):
+    """Represents a class MyList based on the built-in list class."""
+
+    def print_sorted(self):
+        """Prints a sorted form of list in ascending order."""
+        print(sorted(self))
