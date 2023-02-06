a9c638de63959b025610c38388b8cce91fce2176
Messai <messai@github.com>
2023-02-06 16:17:52 +0300

    Started a new project and added 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,4 +3,5 @@
 
 
 def lookup(obj):
+    """Returns the list of available attributes and methods of obj."""
     return dir(obj)
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Defines print_sorted function."""
+
+
+def print_sorted(self):
+    
