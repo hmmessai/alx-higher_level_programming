e08388b7153cead53ea7dc01ce36d45dd5f6e284
Messai <messai@github.com>
2023-02-08 10:59:44 +0300

    Added 10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines a class Student."""
+
+
+class Student:
+    """Representation of a Student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a class instance.
+
+        Args:
+            first_name (str): the first name of student.
+            last_name (str): the last name of student.
+            age (int): the age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student.
+
+        Args:
+            attrs: attributes to represent.
+        """
+        if (type(attrs) == list and 
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
