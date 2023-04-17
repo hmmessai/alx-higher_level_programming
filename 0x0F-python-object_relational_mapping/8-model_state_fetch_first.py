24b7c83bb54d24d62fab5041f3cb2e0eb0857586
Messai <messai@github.com>
2023-04-17 08:12:42 +0300

    Added 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Prints the first State object from the database"""
+from sqlalchemy import (create_engine)
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'\
+            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    print(session[0].id, session[1].name, sep=": ")
