f8fe67cd9a7cc0ce243592dc5ad9512628b3cd48
Messai <messai@github.com>
2023-06-15 19:49:17 +0300

    Started new project and added 0

---
+++ 0x0F-python_object_relational_mapping/0-select_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    argv = sys.argv
+    db = MySQLdb.connect(host='localhost', user=argv[1], passwd=argv[2], database=argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
