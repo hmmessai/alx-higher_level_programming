f1713e9f6aa5d2ad61e55569a9c311e8bac5a184
Messai <messai@github.com>
2023-06-15 20:28:34 +0300

    Added 1 and 2

---
+++ 0x0F-python_object_relational_mapping/1-filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Lists all states with a name starting with N"""
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    a = sys.argv
+    db = MySQLdb.connect(host='localhost', user=a[1], passwd=a[2],
+                         database=a[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
---
+++ 0x0F-python_object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Displays all values in states table where the name matches the argument"""
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    a = sys.argv
+    db = MySQLdb.connect(host='localhost',
+                         user=a[1],
+                         passwd=a[2],
+                         database=a[3],
+                         port=3306)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                     .format(a[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
