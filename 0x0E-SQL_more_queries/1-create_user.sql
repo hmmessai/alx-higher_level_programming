ec38bf98a64d3f511cabe27dcf70806269726c4e
Messai <messai@github.com>
2023-06-16 20:43:18 +0300

    correction

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Creates the MySQL server user user_0d_1
 -- which has all privileges on the server
 -- with password user_0d_1_pwd
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO user_od_1@localhost;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
