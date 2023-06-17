6b8e9ed259a04c2608bfefec09e67b7c8729cfbe
Messai <messai@github.com>
2023-06-17 12:24:37 +0300

    Added 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Lists all cities of the state 
+""" Lists all cities of the state
     that has the same name as the inserted name"""
 
 if __name__ == '__main__':
@@ -9,10 +9,10 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost',
                          user=sys.argv[1],
                          passwd=sys.argv[2],
-                         database = sys.argv[3],
+                         database=sys.argv[3],
                          port=3306)
     cur = db.cursor()
-    cur.execute("""SELECT cities.name 
+    cur.execute("""SELECT cities.name
                 FROM cities
                 INNER JOIN states
                 ON cities.state_id = states.id
