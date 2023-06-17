32162d7fcdb5c8557bdd7ddeeb9e3ef0b843f4de
Messai <messai@github.com>
2023-06-17 13:45:30 +0300

    Correction to 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                     .format(a[4]))
+                .format(a[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
