4116349f4280e888aa49f8cdec2e1e75ffdfc4f7
Messai <messai@github.com>
2023-06-18 18:53:05 +0300

    Added 10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -7,5 +7,11 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
-    engine = create_engine(''
-                           )
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    name = sys.argv[4]
+    instance = session.query(State).filter(State.name.like(name))
+    print(instance.id)
