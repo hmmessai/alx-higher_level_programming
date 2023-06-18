6fc06d8e9cfd82936d764381c0119b73299312fa
Messai <hmmessai@github.com>
2023-06-18 03:46:42 -0700

    Added 7-model_state_fetch_all

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Lists all State objects from the db"""
+import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
