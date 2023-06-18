a3a39fc803cabb558c16517527f30228fabb1aae
Messai <hmmessai@github.com>
2023-06-18 04:31:55 -0700

    Added 10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-""":
+"""Prints the State object with the name passed as argument from db"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine(''
+                           )
