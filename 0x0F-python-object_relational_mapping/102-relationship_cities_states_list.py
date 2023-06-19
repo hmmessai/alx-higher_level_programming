1d6bc6cbc91399238258eca931233c619bababdc
Messai <hmmessai@github.com>
2023-06-19 08:08:32 -0700

    Added 102

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -16,5 +16,6 @@ if __name__ == "__main__":
     session = Session()
     for instance in session.query(State).order_by(State.id):
         for city_ins in instance.cities:
-            print(city_ins.id, city_ins.name, sep=": ", end="")
-            print(" -> " + instance.name)
+            print("{}: {} -> {}".format(city_ins.id,
+                                        city_ins.name,
+                                        instance.name))
