3877af893c87ef7563e5870ce3e055cd9ba8a96e
Messai <messai@github.com>
2023-06-19 10:55:50 +0300

    correction

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -5,8 +5,8 @@ from sqlalchemy import Column, Integer
 
 
 class City(Base):
-    """Representation of the City table 
-    in the corresponding mysql db with 
+    """Representation of the City table
+    in the corresponding mysql db with
     Attributes:
         id: id of the city
         name: name of the city
