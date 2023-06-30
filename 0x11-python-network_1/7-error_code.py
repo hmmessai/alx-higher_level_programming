3998a44ac7c1c7d293678f6b1a2027b0ed5e52ff
Messai <messai@github.com>
2023-06-30 21:53:30 +0300

    Added 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,6 +10,6 @@ if __name__ == '__main__':
     r = requests.get(sys.argv[1])
     try:
         r.raise_for_status()
-        print("Error code: {}".format(r.status_code))
-    except requests.exceptions.HTTPError:
         print(r.text)
+    except requests.exceptions.HTTPError:
+        print("Error code: {}".format(r.status_code))
