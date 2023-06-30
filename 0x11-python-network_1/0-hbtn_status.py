212ed48d52d163c5d8aa0c8031f190879aec59de
Messai <messai@github.com>
2023-06-30 20:30:57 +0300

    0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@
 
 if __name__ == '__main__':
     import urllib.request
-    
+
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         content = res.read()
         print("Body response:")
