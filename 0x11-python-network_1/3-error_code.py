80d9ff26cbcb4b6999fae2fc40eaa0c8e4eae443
Messai <messai@github.com>
2023-06-30 21:25:31 +0300

    Added 4

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,8 +8,8 @@ import urllib.request
 import urllib.error
 
 if __name__ == '__main__':
-        try:
-            with urllib.request.urlopen(sys.argv[1]) as res:
-                print(res.read().decode('UTF-8'))
-        except urllib.error.HTTPError as e:
-            print("Error code: {}".format(e.code))
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode('UTF-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
