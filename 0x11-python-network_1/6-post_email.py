8a64be3cae9ab262dd59d9391305bc28a4f87f96
Messai <messai@github.com>
2023-07-01 07:09:01 +0300

    Added 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,6 +8,5 @@ import requests
 import sys
 
 if __name__ == '__main__':
-    data = {'email': sys.argv[2]}
-    r = requests.post(sys.argv[1], data)
+    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
     print(r.text)
