90a3425c2a00814fb960ded05e31231590ff54dc
Messai <messai@github.com>
2023-07-04 16:55:48 +0300

    correction

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """
 Takes Github credentials (username and password)
-and displays the id 
+and displays the id
 """
 import sys
 import requests
 
 if __name__ == "__main__":
     payload = {'Authorization': "{}".format(sys.argv[2])}
-    r = requests.get('https://api.github.com/users/{}'.format(sys.argv[1]), headers=payload)
+    r = requests.get('https://api.github.com/users/{}'.format(sys.argv[1]),
+                     headers=payload)
     json = r.json()
 
     print(json['id'])
