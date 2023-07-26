ed121bc1a057640f54d7868e149361523bc02ad4
Messai <messai@github.com>
2023-07-26 22:17:13 +0300

    6

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,23 +1,4 @@
 #!/usr/bin/node
-const request = require('request');
 const fs = require('fs');
-
-const url = process.argv[2];
-const filePath = process.argv[3];
-
-request(url, function (error, response, body) {
-  if (error) {
-    console.error(error);
-    return;
-  }
-
-  fs.writeFile(filePath, body, { encoding: 'utf-8' }, function (error) {
-    if (error) {
-      console.error(error);
-      return;
-    }
-
-    console.log(`The contents of ${url} have been saved to ${filePath}`);
-  });
-});
-
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
